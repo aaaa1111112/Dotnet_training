71a183ea431d13f06cbda910e5de8702db3f5399
aaaa1111112 <94889132+aaaa1111112@users.noreply.github.com>
2022-04-13 11:06:46 +0530

    Add files via upload

---
+++ MVCNEW/MVCNEW/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCNEW.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Fname { get; set; }
+        public string? Lname { get; set; }
+        public string? Roll { get; set; }
+    }
+}
